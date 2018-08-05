730a6f7ce3d1af77ae4dd2a42435d0ee82107e21
Mathias Minder <Mathias.Minder@diartis.ch>
2018-08-05 16:18:51 +0200

    upgrade participants to Angular 5

--- EventRegistrator.Web/ClientApp/src/app/participants/participants.component.ts
+++ EventRegistrator.Web/ClientApp/src/app/participants/participants.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject, } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'participants',
@@ -10,48 +10,56 @@ export class ParticipantsComponent {
   public registrable: Registrable;
   registrableId: any;
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string, private router: Router, private route: ActivatedRoute) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.registrableId = this.route.snapshot.params['id'];
+    this.registrableId = this.route.snapshot.params["id"];
     this.refreshParticipants();
   }
 
   refreshParticipants() {
-    this.http.get<Registrable>(`${this.baseUrl}api/registrables/${this.registrableId}/participants`)
+    this.http.get<Registrable>(`api/events/${this.getEventAcronym()}/registrables/${this.getRegistrableId()}/participants`)
       .subscribe(result => { this.registrable = result; },
         error => console.error(error));
   }
 
   promote() {
-    this.http.post(`${this.baseUrl}api/registrables/${this.registrableId}/tryPromoteFromWaitingList`, null)
+    this.http.post(`api/events/${this.getEventAcronym()}/registrables/${this.getRegistrableId()}/tryPromoteFromWaitingList`, null)
       .subscribe(result => { },
         error => console.error(error));
   }
+
+  getEventAcronym() {
+    return this.route.snapshot.params["eventAcronym"];
+  }
+
+  getRegistrableId() {
+    return this.route.snapshot.params["id"];
+  }
 }
 
-interface Registrable {
-  Name: string;
-  MaximumSingleSeats: number;
-  MaximumDoubleSeats: number;
-  MaximumAllowedImbalance: number;
-  HasWaitingList: boolean;
-  Participants: Place[];
-  WaitingList: Place[];
+class Registrable {
+  name: string;
+  maximumSingleSeats: number;
+  maximumDoubleSeats: number;
+  maximumAllowedImbalance: number;
+  hasWaitingList: boolean;
+  participants: Place[];
+  waitingList: Place[];
 }
 
-interface Place {
-  Leader: Registration;
-  Follower: Registration;
-  IsOnWaitingList: boolean;
-  IsPartnerRegistration: boolean;
+class Place {
+  leader: Registration;
+  follower: Registration;
+  isOnWaitingList: boolean;
+  isPartnerRegistration: boolean;
 }
 
-interface Registration {
-  Id: string;
-  FirstName: string;
-  LastName: string;
-  State: number;
-  Email: string;
+class Registration {
+  id: string;
+  firstName: string;
+  lastName: string;
+  state: number;
+  email: string;
 }
