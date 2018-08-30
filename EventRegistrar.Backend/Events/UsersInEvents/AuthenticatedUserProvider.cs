0f4e0c61f7b1d1873b7b5af594e48694141bbeda
Mathias Minder <Mathias.Minder@diartis.ch>
2018-08-31 00:03:45 +0200

    Revert "fix paths"
    This reverts commit 196467e8a0857d948855b208546821d3adc8d7e0.

--- EventRegistrar.Backend/Events/UsersInEvents/AuthenticatedUserProvider.cs
+++ EventRegistrar.Backend/Events/UsersInEvents/AuthenticatedUserProvider.cs
@@ -35,7 +35,7 @@ namespace EventRegistrar.Backend.Events.UsersInEvents
                                                             && usr.IdentityProviderUserIdentifier == identifier);
             if (user == null)
             {
-                return new Guid("73B167CE-61CC-46AC-BC7D-F72A1EA5D7C9");
+                //return new Guid("73B167CE-61CC-46AC-BC7D-F72A1EA5D7C9");
                 return null;
             }
 
