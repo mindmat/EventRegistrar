b4c7cb70869c03a1b04a42d35333333a7c6b7b83
Mathias Minder <Mathias.Minder@diartis.ch>
2018-10-09 23:53:18 +0200

    map MailEvent to table MailEvents

--- EventRegistrar.Backend/Mailing/Feedback/MailEventMap.cs
+++ EventRegistrar.Backend/Mailing/Feedback/MailEventMap.cs
@@ -1,8 +1,15 @@
 ﻿using EventRegistrar.Backend.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace EventRegistrar.Backend.Mailing.Feedback
 {
     public class MailEventMap : EntityTypeConfiguration<MailEvent>
     {
+        public override void Configure(EntityTypeBuilder<MailEvent> builder)
+        {
+            builder.ToTable("MailEvents");
+            base.Configure(builder);
+        }
     }
 }
