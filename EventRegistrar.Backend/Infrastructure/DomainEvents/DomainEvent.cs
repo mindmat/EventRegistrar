ee67121348bdbaa249ade168813f825e340dee48
Mathias Minder <Mathias.Minder@diartis.ch>
2018-11-10 16:33:53 +0100

    Parent of event can be nullable

--- EventRegistrar.Backend/Infrastructure/DomainEvents/DomainEvent.cs
+++ EventRegistrar.Backend/Infrastructure/DomainEvents/DomainEvent.cs
@@ -4,7 +4,7 @@ namespace EventRegistrar.Backend.Infrastructure.DomainEvents
 {
     public class DomainEvent
     {
-        public Guid DomainEventId_Parent { get; set; }
+        public Guid? DomainEventId_Parent { get; set; }
         public Guid? EventId { get; set; }
         public Guid Id { get; set; }
     }
