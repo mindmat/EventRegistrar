eedb1ff39d02b81af39c6f820dbf1212ea128e95
Mathias Minder <Mathias.Minder@diartis.ch>
2018-12-24 02:38:56 +0100

    resend acceptance mails, not reminders

--- EventRegistrar.Backend/Mailing/InvalidAddresses/ResendLastMailWhenEmailAddressFixed.cs
+++ EventRegistrar.Backend/Mailing/InvalidAddresses/ResendLastMailWhenEmailAddressFixed.cs
@@ -2,17 +2,21 @@
 using System.Linq;
 using EventRegistrar.Backend.Infrastructure.DomainEvents;
 using EventRegistrar.Backend.Mailing.Compose;
+using EventRegistrar.Backend.Payments.Due;
 using MediatR;
 
 namespace EventRegistrar.Backend.Mailing.InvalidAddresses
 {
     public class ResendLastMailWhenEmailAddressFixed : IEventToCommandTranslation<InvalidEmailAddressFixed>
     {
+        private readonly DuePaymentConfiguration _duePaymentConfiguration;
         private readonly IQueryable<MailToRegistration> _mails;
 
-        public ResendLastMailWhenEmailAddressFixed(IQueryable<MailToRegistration> mails)
+        public ResendLastMailWhenEmailAddressFixed(IQueryable<MailToRegistration> mails,
+                                                   DuePaymentConfiguration duePaymentConfiguration)
         {
             _mails = mails;
+            _duePaymentConfiguration = duePaymentConfiguration;
         }
 
         public IEnumerable<IRequest> Translate(InvalidEmailAddressFixed e)
@@ -20,7 +24,9 @@ namespace EventRegistrar.Backend.Mailing.InvalidAddresses
             if (e.EventId.HasValue)
             {
                 var lastMail = _mails.Where(mail => mail.RegistrationId == e.RegistrationId
-                                                 && mail.Mail.Type.HasValue)
+                                                 && mail.Mail.Type.HasValue
+                                                 && !_duePaymentConfiguration.MailTypes_Reminder1.Contains(mail.Mail.Type.Value)
+                                                 && !_duePaymentConfiguration.MailTypes_Reminder2.Contains(mail.Mail.Type.Value))
                                      .OrderByDescending(mail => mail.Mail.Sent)
                                      .First();
 
