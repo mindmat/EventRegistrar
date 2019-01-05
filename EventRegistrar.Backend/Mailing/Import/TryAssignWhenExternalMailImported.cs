d4e5a8eeb29c9528dbdf12451ce7f864beeb9b6e
Mathias Minder <Mathias.Minder@diartis.ch>
2019-01-05 17:51:53 +0100

    try assign imported mail

---
+++ EventRegistrar.Backend/Mailing/Import/TryAssignWhenExternalMailImported.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EventRegistrar.Backend.Infrastructure.DomainEvents;
+using MediatR;
+
+namespace EventRegistrar.Backend.Mailing.Import
+{
+    public class TryAssignWhenExternalMailImported : IEventToCommandTranslation<ExternalMailImported>
+    {
+        public IEnumerable<IRequest> Translate(ExternalMailImported e)
+        {
+            yield return new TryAssignImportedMailCommand { ImportedMailId = e.ImportedMailId };
+        }
+    }
+}
