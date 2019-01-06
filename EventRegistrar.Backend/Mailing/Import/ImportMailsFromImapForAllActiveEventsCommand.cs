fd393a94b3c794c5f51312788e84605dd4b4b6dc
Mathias Minder <Mathias.Minder@diartis.ch>
2019-01-06 21:35:57 +0100

    Periodically import mails from IMAP config

---
+++ EventRegistrar.Backend/Mailing/Import/ImportMailsFromImapForAllActiveEventsCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventRegistrar.Backend.Infrastructure.Configuration;
+using EventRegistrar.Backend.Infrastructure.ServiceBus;
+using EventRegistrar.Backend.RegistrationForms;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventRegistrar.Backend.Mailing.Import
+{
+    public class ImportMailsFromImapForAllActiveEventsCommand : IRequest
+    {
+    }
+
+    public class ImportMailsFromImapForAllActiveEventsCommandHandler : IRequestHandler<ImportMailsFromImapForAllActiveEventsCommand>
+    {
+        private readonly IQueryable<EventConfiguration> _configurations;
+        private readonly ServiceBusClient _serviceBus;
+
+        public ImportMailsFromImapForAllActiveEventsCommandHandler(IQueryable<EventConfiguration> configurations,
+                                                                   ServiceBusClient serviceBus)
+        {
+            _configurations = configurations;
+            _serviceBus = serviceBus;
+        }
+
+        public async Task<Unit> Handle(ImportMailsFromImapForAllActiveEventsCommand command, CancellationToken cancellationToken)
+        {
+            var activeImportConfigurations = await _configurations.Where(cfg => cfg.Event.State != State.Finished
+                                                                             && cfg.Type == typeof(ExternalMailConfiguration).FullName)
+                                                                  .ToListAsync(cancellationToken);
+            foreach (var activeImportConfiguration in activeImportConfigurations)
+            {
+                _serviceBus.SendMessage(new ImportMailsFromImapCommand { EventId = activeImportConfiguration.EventId });
+            }
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ EventRegistrar.Functions/ImportMailTrigger.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace EventRegistrar.Functions
+{
+    public static class ImportMailTrigger
+    {
+        [FunctionName("ImportMailTrigger")]
+        public static async Task Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var command = new
+            {
+                CommandType = "EventRegistrar.Backend.Mailing.Import.ImportMailsFromImapForAllActiveEventsCommand",
+                CommandSerialized = new { }
+            };
+            await ServiceBusClient.SendCommand(command, "CommandQueue");
+        }
+    }
+}
