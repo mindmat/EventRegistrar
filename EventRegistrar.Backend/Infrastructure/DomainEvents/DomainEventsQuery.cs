812339975e82c5fe773a9c404dd9cde7f4e679a6
Mathias Minder <Mathias.Minder@diartis.ch>
2019-09-21 12:40:02 +0200

    DomainEvents-Query prepared

---
+++ EventRegistrar.Backend/Infrastructure/DomainEvents/DomainEventsQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventRegistrar.Backend.Authorization;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventRegistrar.Backend.Infrastructure.DomainEvents
+{
+    public class DomainEventDisplayItem
+    {
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+
+    public class DomainEventsQuery : IRequest<IEnumerable<DomainEventDisplayItem>>, IEventBoundRequest
+    {
+        public Guid EventId { get; set; }
+    }
+
+    public class DomainEventsQueryHandler : IRequestHandler<DomainEventsQuery, IEnumerable<DomainEventDisplayItem>>
+    {
+        private readonly IQueryable<PersistedDomainEvent> _domainEvents;
+
+        public DomainEventsQueryHandler(IQueryable<PersistedDomainEvent> domainEvents)
+        {
+            _domainEvents = domainEvents;
+        }
+
+        public async Task<IEnumerable<DomainEventDisplayItem>> Handle(DomainEventsQuery query, CancellationToken cancellationToken)
+        {
+            return await _domainEvents.Where(evt => evt.EventId == query.EventId)
+                                      .Take(100)
+                                      .Select(evt => new DomainEventDisplayItem
+                                      {
+                                          Timestamp = evt.Timestamp,
+                                          //evt.
+                                      })
+                                      .ToListAsync();
+        }
+    }
+}
---
+++ EventRegistrar.Backend/Infrastructure/DomainEvents/HostingController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using EventRegistrar.Backend.Events;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventRegistrar.Backend.Hosting
+{
+    public class DomainEventsController : Controller
+    {
+        private readonly IEventAcronymResolver _eventAcronymResolver;
+        private readonly IMediator _mediator;
+
+        public DomainEventsController(IMediator mediator,
+                                      IEventAcronymResolver eventAcronymResolver)
+        {
+            _mediator = mediator;
+            _eventAcronymResolver = eventAcronymResolver;
+        }
+
+        [HttpGet("api/events/{eventAcronym}/domainevents")]
+        public async Task<HostingOffers> GetRecentEvent(string eventAcronym)
+        {
+            return await _mediator.Send(new HostingOffersQuery
+            {
+                EventId = await _eventAcronymResolver.GetEventIdFromAcronym(eventAcronym)
+            });
+        }
+    }
+}
