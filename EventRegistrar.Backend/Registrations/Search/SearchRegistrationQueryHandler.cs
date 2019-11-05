ffe47aea3f0b8a66b318a298656ab3fd2a13c692
Mathias Minder <Mathias.Minder@diartis.ch>
2019-11-05 19:18:42 +0100

    fix error in search when Email is missing

--- EventRegistrar.Backend/Registrations/Search/SearchRegistrationQueryHandler.cs
+++ EventRegistrar.Backend/Registrations/Search/SearchRegistrationQueryHandler.cs
@@ -22,18 +22,18 @@ namespace EventRegistrar.Backend.Registrations.Search
             var allowedStates = query.States.Any() ? query.States : new[] { RegistrationState.Received };
             var searchString = query.SearchString?.Trim();
             var registrations = await _registrations.Where(reg => reg.EventId == query.EventId
-                                                                  && (reg.RespondentFirstName.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
-                                                                   || reg.RespondentLastName.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
-                                                                   || reg.RespondentEmail.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
-                                                                   || reg.PhoneNormalized.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
-                                                                  && allowedStates.Contains(reg.State))
+                                                               && (reg.RespondentFirstName.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                                                                || reg.RespondentLastName.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                                                                || reg.RespondentEmail.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                                                                || reg.PhoneNormalized != null && reg.PhoneNormalized.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                                                               && allowedStates.Contains(reg.State))
                                                     .Select(reg => new RegistrationMatch
                                                     {
                                                         RegistrationId = reg.Id,
                                                         FirstName = reg.RespondentFirstName,
                                                         LastName = reg.RespondentLastName,
                                                         Amount = reg.Price ?? 0m,
-                                                        AmountPaid = reg.Payments.Sum(pmt => pmt.Amount),
+                                                        AmountPaid = reg.Payments.Select(pmt => pmt.Amount).DefaultIfEmpty(0m).Sum(),
                                                         State = reg.State,
                                                         StateText = reg.State.ToString(),
                                                         IsWaitingList = reg.IsWaitingList == true
