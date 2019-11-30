50eebf522ecdbdb8aea3f1dd13b0ea02b416d06e
Mathias Minder <Mathias.Minder@diartis.ch>
2019-11-30 22:03:18 +0100

    keep search string through url query param

--- EventRegistrator.Web/ClientApp/src/app/registration/searchRegistration.component.ts
+++ EventRegistrator.Web/ClientApp/src/app/registration/searchRegistration.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'searchRegistration',
@@ -9,14 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 export class SearchRegistrationComponent {
   registrations: Registration[];
   isSearching: boolean;
+  searchString: string;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
     this.isSearching = false;
+    var searchParam = route.snapshot.queryParams['search'];
+    if (searchParam != null) {
+      this.searchString = searchParam;
+      this.search();
+    }
   }
 
-  search(searchString: string) {
+  search() {
     this.isSearching = true;
-    this.http.get<Registration[]>(`api/events/${this.getEventAcronym()}/registrations?searchstring=${searchString}&states=received&states=paid&states=cancelled`)
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: { search: this.searchString },
+        queryParamsHandling: 'merge'
+      });
+    this.http.get<Registration[]>(`api/events/${this.getEventAcronym()}/registrations?searchstring=${this.searchString}&states=received&states=paid&states=cancelled`)
       .subscribe(result => {
         this.registrations = result;
         this.isSearching = false;
