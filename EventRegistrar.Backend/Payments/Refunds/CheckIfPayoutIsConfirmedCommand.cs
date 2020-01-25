ef6b608ae4d85ed6c35af27e00dab9726ad449ad
Mathias Minder <mathias.minder@outlook.com>
2020-01-25 14:22:22 +0100

    CheckIfPayoutIsConfirmedCommand

---
+++ EventRegistrar.Backend/Payments/Refunds/CheckIfPayoutIsConfirmedCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using EventRegistrar.Backend.Infrastructure.DataAccess;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EventRegistrar.Backend.Payments.Refunds
+{
+    public class CheckIfPayoutIsConfirmedCommand : IRequest
+    {
+        public Guid PayoutRequestId { get; set; }
+    }
+
+    public class CheckIfPayoutIsConfirmedCommandHandler : IRequestHandler<CheckIfPayoutIsConfirmedCommand>
+    {
+        private readonly IRepository<PayoutRequest> _payoutRequests;
+
+        public CheckIfPayoutIsConfirmedCommandHandler(IRepository<PayoutRequest> payoutRequests)
+        {
+            _payoutRequests = payoutRequests;
+        }
+
+        public async Task<Unit> Handle(CheckIfPayoutIsConfirmedCommand command, CancellationToken cancellationToken)
+        {
+            var payoutRequest = await _payoutRequests.Where(pmt => pmt.Id == command.PayoutRequestId)
+                                                     .Include(pmt => pmt.Assignments)
+                                                     .FirstAsync(cancellationToken);
+            var balance = payoutRequest.Amount
+                        - payoutRequest.Assignments.Sum(asn => asn.Amount);
+            payoutRequest.State = balance == 0m ? PayoutState.Confirmed : PayoutState.Requested;
+            return Unit.Value;
+        }
+    }
+}
---
+++ EventRegistrar.Backend/Payments/Refunds/CheckIfPayoutIsConfirmedWhenPaymentAssigned.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using EventRegistrar.Backend.Infrastructure.DomainEvents;
+using EventRegistrar.Backend.Payments.Assignments;
+
+using MediatR;
+
+namespace EventRegistrar.Backend.Payments.Refunds
+{
+    public class CheckIfPayoutIsConfirmedWhenPaymentAssigned : IEventToCommandTranslation<PaymentAssigned>
+    {
+        public IEnumerable<IRequest> Translate(PaymentAssigned e)
+        {
+            if (e.PayoutRequestId != null)
+            {
+                yield return new CheckIfPayoutIsConfirmedCommand { PayoutRequestId = e.PayoutRequestId.Value };
+            }
+        }
+    }
+}
