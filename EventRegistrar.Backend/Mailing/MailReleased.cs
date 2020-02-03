2dbeb4409993f9d7af5fca88503ee4ebeb5dfecb
Mathias Minder <mathias.minder@outlook.com>
2020-02-03 23:32:24 +0100

    Properties MailReleased public for deserialization

--- EventRegistrar.Backend/Mailing/MailReleased.cs
+++ EventRegistrar.Backend/Mailing/MailReleased.cs
@@ -6,8 +6,8 @@ namespace EventRegistrar.Backend.Mailing
 {
     public class MailReleased : DomainEvent
     {
-        public Guid MailId { get; internal set; }
-        public string To { get; internal set; }
+        public Guid MailId { get; set; }
+        public string To { get; set; }
         public string Subject { get; set; }
     }
 
