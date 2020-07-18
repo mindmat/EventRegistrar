66af4196fba3482ed9aade8b31f236811cc8b120
Mathias Minder <mathias.minder@outlook.com>
2020-07-19 01:32:41 +0200

    wip: Organizer app

---
+++ EventRegistrar.OrganizerApp/EventRegistrar.OrganizerApp/Services/SocialAuthenticator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+
+namespace EventRegistrar.OrganizerApp.Services
+{
+    public class SocialAuthenticator
+    {
+        public async Task Authenticate()
+        {
+            var authResult = await WebAuthenticator.AuthenticateAsync(
+                new Uri("https://eventregistrar.azurewebsites.net/.auth/login/google/callback"),
+                new Uri("eventregistrar://"));
+
+            var accessToken = authResult?.AccessToken;
+        }
+    }
+}
---
+++ EventRegistrar.OrganizerApp/EventRegistrar.OrganizerApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EventRegistrar.OrganizerApp.Models;
+
+namespace EventRegistrar.OrganizerApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
