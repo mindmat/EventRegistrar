177eac615ec510af4ff5635c3ae64635f6b6faef
Mathias Minder <mathias.minder@outlook.com>
2021-08-15 22:48:43 +0200

    script to setup db

---
+++ src/EventRegistrar.Backend/Infrastructure/DataAccess/Migrations/setup db.sql
@@ -0,0 +1,9 @@
+﻿CREATE USER [eventregistrator] FOR LOGIN [eventregistrator] WITH DEFAULT_SCHEMA=[dbo]
+GO
+sys.sp_addrolemember @rolename = N'db_owner', @membername = N'eventregistrator'
+GO
+sys.sp_addrolemember @rolename = N'db_datareader', @membername = N'eventregistrator'
+GO
+sys.sp_addrolemember @rolename = N'db_datawriter', @membername = N'eventregistrator'
+GO
+grant select, update, delete, insert to eventregistrator 
