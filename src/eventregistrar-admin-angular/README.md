a23e0fc7de0e82258c8f6192ab759526c45795db
Mathias Minder <mathias.minder@outlook.com>
2021-12-04 20:37:31 +0100

    Reset Frontend: Use Fuse template

--- src/eventregistrar-admin-angular/README.md
+++ src/eventregistrar-admin-angular/README.md
@@ -1,6 +1,6 @@
-# EventregistrarAdminAngular
+# Fuse - Admin template and Starter project for Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli)
 
 ## Development server
 
--- src/eventregistrar-admin-angular/src/app/app.component.ts
+++ src/eventregistrar-admin-angular/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector   : 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls  : ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'eventregistrar-admin-angular';
+export class AppComponent
+{
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
 }
