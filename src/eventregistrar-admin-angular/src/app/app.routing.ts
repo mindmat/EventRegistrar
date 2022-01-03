0d0718e8f477aecc990cfe9fdbcd6b050f1e30ab
Mathias Minder <mathias.minder@gmail.com>
2022-01-03 11:48:43 +0100

    remove example

--- src/eventregistrar-admin-angular/src/app/app.routing.ts
+++ src/eventregistrar-admin-angular/src/app/app.routing.ts
@@ -14,15 +14,15 @@ import { ParticipantsSingleComponent } from './modules/admin/participants/partic
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
 
-    // Redirect empty path to '/example'
-    { path: '', pathMatch: 'full', redirectTo: 'example' },
+    // Redirect empty path to '/overview'
+    { path: '', pathMatch: 'full', redirectTo: 'overview' },
 
-    // Redirect signed in user to the '/example'
+    // Redirect signed in user to the '/overview'
     //
     // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
     // path. Below is another redirection for that path to redirect the user to the desired
     // location. This is a small convenience to keep all main routes together here on this file.
-    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'example' },
+    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'overview' },
 
     // Auth routes for guests
     {
@@ -79,7 +79,6 @@ export const appRoutes: Route[] = [
             initialData: InitialDataResolver,
         },
         children: [
-            { path: 'example', loadChildren: () => import('app/modules/admin/example/example.module').then(m => m.ExampleModule) },
             {
                 path: 'overview',
                 children: [
--- src/eventregistrar-admin-angular/src/app/mock-api/common/navigation/data.ts
+++ src/eventregistrar-admin-angular/src/app/mock-api/common/navigation/data.ts
@@ -3,44 +3,37 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    },
-    {
-        id   : 'overview',
+        id: 'overview',
         title: 'Übersicht',
-        type : 'basic',
-        icon : 'heroicons_outline:clipboard-check',
-        link : '/overview'
+        type: 'basic',
+        icon: 'heroicons_outline:clipboard-check',
+        link: '/overview'
     },
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
     }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
     }
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
     }
 ];
