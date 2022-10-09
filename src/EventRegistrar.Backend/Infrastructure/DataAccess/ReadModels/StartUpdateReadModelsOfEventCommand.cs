7a43421b8e5db210812180a2ee090cc8492eb5bc
Mathias Minder <mathias.minder@gmail.com>
2022-10-09 21:32:33 +0200

    StartUpdateReadModelsOfEventCommand for all events

--- src/EventRegistrar.Backend/Infrastructure/DataAccess/ReadModels/StartUpdateReadModelsOfEventCommand.cs
+++ src/EventRegistrar.Backend/Infrastructure/DataAccess/ReadModels/StartUpdateReadModelsOfEventCommand.cs
@@ -1,4 +1,5 @@
-﻿using EventRegistrar.Backend.Infrastructure.ServiceBus;
+﻿using EventRegistrar.Backend.Events;
+using EventRegistrar.Backend.Infrastructure.ServiceBus;
 using EventRegistrar.Backend.Payments.Assignments;
 using EventRegistrar.Backend.Payments.Due;
 using EventRegistrar.Backend.Payments.Files;
@@ -9,23 +10,26 @@ namespace EventRegistrar.Backend.Infrastructure.DataAccess.ReadModels;
 
 public class StartUpdateReadModelsOfEventCommand : IRequest
 {
-    public Guid EventId { get; set; }
+    public Guid? EventId { get; set; }
     public IEnumerable<string>? QueryNames { get; set; }
 }
 
 public class StartUpdateReadModelsOfEventCommandHandler : IRequestHandler<StartUpdateReadModelsOfEventCommand>
 {
     private readonly CommandQueue _commandQueue;
+    private readonly IQueryable<Event> _events;
     private readonly IQueryable<Registration> _registrations;
     private readonly IQueryable<Payment> _payments;
     private readonly IDateTimeProvider _dateTimeProvider;
 
     public StartUpdateReadModelsOfEventCommandHandler(CommandQueue commandQueue,
+                                                      IQueryable<Event> events,
                                                       IQueryable<Registration> registrations,
                                                       IQueryable<Payment> payments,
                                                       IDateTimeProvider dateTimeProvider)
     {
         _commandQueue = commandQueue;
+        _events = events;
         _registrations = registrations;
         _payments = payments;
         _dateTimeProvider = dateTimeProvider;
@@ -33,59 +37,65 @@ public class StartUpdateReadModelsOfEventCommandHandler : IRequestHandler<StartU
 
     public async Task<Unit> Handle(StartUpdateReadModelsOfEventCommand command, CancellationToken cancellationToken)
     {
-        if (command.QueryNames?.Contains(nameof(RegistrablesOverviewQuery)) != false)
+        var eventIds = await _events.WhereIf(command.EventId != null, evt => evt.Id == command.EventId)
+                                    .Select(evt => evt.Id)
+                                    .ToListAsync(cancellationToken);
+        foreach (var eventId in eventIds)
         {
-            _commandQueue.EnqueueCommand(new UpdateReadModelCommand
-                                         {
-                                             QueryName = nameof(RegistrablesOverviewQuery),
-                                             EventId = command.EventId,
-                                             DirtyMoment = _dateTimeProvider.Now
-                                         });
-        }
-
-        if (command.QueryNames?.Contains(nameof(DuePaymentsQuery)) != false)
-        {
-            _commandQueue.EnqueueCommand(new UpdateReadModelCommand
-                                         {
-                                             QueryName = nameof(DuePaymentsQuery),
-                                             EventId = command.EventId,
-                                             DirtyMoment = _dateTimeProvider.Now
-                                         });
-        }
-
-        if (command.QueryNames?.Contains(nameof(RegistrationQuery)) != false)
-        {
-            var registrationIds = await _registrations.Where(reg => reg.EventId == command.EventId)
-                                                      .Select(reg => reg.Id)
-                                                      .ToListAsync(cancellationToken);
-
-            foreach (var registrationId in registrationIds)
+            if (command.QueryNames?.Contains(nameof(RegistrablesOverviewQuery)) != false)
             {
                 _commandQueue.EnqueueCommand(new UpdateReadModelCommand
                                              {
-                                                 QueryName = nameof(RegistrationQuery),
-                                                 EventId = command.EventId,
-                                                 RowId = registrationId,
+                                                 QueryName = nameof(RegistrablesOverviewQuery),
+                                                 EventId = eventId,
                                                  DirtyMoment = _dateTimeProvider.Now
                                              });
             }
-        }
 
-        if (command.QueryNames?.Contains(nameof(PaymentAssignmentsQuery)) != false)
-        {
-            var paymentIds = await _payments.Where(pmt => pmt.PaymentsFile!.EventId == command.EventId)
-                                            .Select(pmt => pmt.Id)
-                                            .ToListAsync(cancellationToken);
-
-            foreach (var paymentId in paymentIds)
+            if (command.QueryNames?.Contains(nameof(DuePaymentsQuery)) != false)
             {
                 _commandQueue.EnqueueCommand(new UpdateReadModelCommand
                                              {
-                                                 QueryName = nameof(PaymentAssignmentsQuery),
-                                                 EventId = command.EventId,
-                                                 RowId = paymentId,
+                                                 QueryName = nameof(DuePaymentsQuery),
+                                                 EventId = eventId,
                                                  DirtyMoment = _dateTimeProvider.Now
                                              });
+            }
+
+            if (command.QueryNames?.Contains(nameof(RegistrationQuery)) != false)
+            {
+                var registrationIds = await _registrations.Where(reg => reg.EventId == command.EventId)
+                                                          .Select(reg => reg.Id)
+                                                          .ToListAsync(cancellationToken);
+
+                foreach (var registrationId in registrationIds)
+                {
+                    _commandQueue.EnqueueCommand(new UpdateReadModelCommand
+                                                 {
+                                                     QueryName = nameof(RegistrationQuery),
+                                                     EventId = eventId,
+                                                     RowId = registrationId,
+                                                     DirtyMoment = _dateTimeProvider.Now
+                                                 });
+                }
+            }
+
+            if (command.QueryNames?.Contains(nameof(PaymentAssignmentsQuery)) != false)
+            {
+                var paymentIds = await _payments.Where(pmt => pmt.PaymentsFile!.EventId == command.EventId)
+                                                .Select(pmt => pmt.Id)
+                                                .ToListAsync(cancellationToken);
+
+                foreach (var paymentId in paymentIds)
+                {
+                    _commandQueue.EnqueueCommand(new UpdateReadModelCommand
+                                                 {
+                                                     QueryName = nameof(PaymentAssignmentsQuery),
+                                                     EventId = eventId,
+                                                     RowId = paymentId,
+                                                     DirtyMoment = _dateTimeProvider.Now
+                                                 });
+                }
             }
         }
 
