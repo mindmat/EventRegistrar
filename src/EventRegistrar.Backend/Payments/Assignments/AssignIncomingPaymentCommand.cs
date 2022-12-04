4620d37ad1679dab67c129b6b15370d4d7219e3b
Mathias Minder <mathias.minder@gmail.com>
2022-12-04 23:35:55 +0100

    update registration view after payment assignment

--- src/EventRegistrar.Backend/Payments/Assignments/AssignIncomingPaymentCommand.cs
+++ src/EventRegistrar.Backend/Payments/Assignments/AssignIncomingPaymentCommand.cs
@@ -1,10 +1,12 @@
 ﻿using EventRegistrar.Backend.Events.UsersInEvents;
 using EventRegistrar.Backend.Infrastructure;
 using EventRegistrar.Backend.Infrastructure.DataAccess;
+using EventRegistrar.Backend.Infrastructure.DataAccess.ReadModels;
 using EventRegistrar.Backend.Infrastructure.DomainEvents;
 using EventRegistrar.Backend.Payments.Files;
 using EventRegistrar.Backend.Registrations;
 using EventRegistrar.Backend.Registrations.IndividualReductions;
+using EventRegistrar.Backend.Registrations.ReadModels;
 
 namespace EventRegistrar.Backend.Payments.Assignments;
 
@@ -27,6 +29,7 @@ public class AssignIncomingPaymentCommandHandler : IRequestHandler<AssignIncomin
     private readonly IQueryable<Registration> _registrations;
     private readonly AuthenticatedUserId _userId;
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly ReadModelUpdater _readModelUpdater;
 
     public AssignIncomingPaymentCommandHandler(IQueryable<Registration> registrations,
                                                IQueryable<IncomingPayment> incomingPayments,
@@ -34,7 +37,8 @@ public class AssignIncomingPaymentCommandHandler : IRequestHandler<AssignIncomin
                                                IRepository<IndividualReduction> individualReductions,
                                                IEventBus eventBus,
                                                AuthenticatedUserId userId,
-                                               IDateTimeProvider dateTimeProvider)
+                                               IDateTimeProvider dateTimeProvider,
+                                               ReadModelUpdater readModelUpdater)
     {
         _registrations = registrations;
         _incomingPayments = incomingPayments;
@@ -43,6 +47,7 @@ public class AssignIncomingPaymentCommandHandler : IRequestHandler<AssignIncomin
         _eventBus = eventBus;
         _userId = userId;
         _dateTimeProvider = dateTimeProvider;
+        _readModelUpdater = readModelUpdater;
     }
 
     public async Task<Unit> Handle(AssignIncomingPaymentCommand command, CancellationToken cancellationToken)
@@ -91,6 +96,8 @@ public class AssignIncomingPaymentCommandHandler : IRequestHandler<AssignIncomin
                               RegistrationId = registration.Id,
                               IncomingPaymentId = incomingPayment.Id
                           });
+
+        _readModelUpdater.TriggerUpdate<RegistrationCalculator>(registration.Id, registration.EventId);
 
         return Unit.Value;
     }
--- src/EventRegistrar.Backend/Payments/Assignments/UnassignPaymentCommand.cs
+++ src/EventRegistrar.Backend/Payments/Assignments/UnassignPaymentCommand.cs
@@ -1,6 +1,8 @@
 ﻿using EventRegistrar.Backend.Infrastructure;
 using EventRegistrar.Backend.Infrastructure.DataAccess;
+using EventRegistrar.Backend.Infrastructure.DataAccess.ReadModels;
 using EventRegistrar.Backend.Infrastructure.DomainEvents;
+using EventRegistrar.Backend.Registrations.ReadModels;
 
 namespace EventRegistrar.Backend.Payments.Assignments;
 
@@ -15,19 +17,23 @@ public class UnassignPaymentCommandHandler : IRequestHandler<UnassignPaymentComm
     private readonly IRepository<PaymentAssignment> _assignments;
     private readonly IEventBus _eventBus;
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly ReadModelUpdater _readModelUpdater;
 
     public UnassignPaymentCommandHandler(IRepository<PaymentAssignment> assignments,
                                          IEventBus eventBus,
-                                         IDateTimeProvider dateTimeProvider)
+                                         IDateTimeProvider dateTimeProvider,
+                                         ReadModelUpdater readModelUpdater)
     {
         _assignments = assignments;
         _eventBus = eventBus;
         _dateTimeProvider = dateTimeProvider;
+        _readModelUpdater = readModelUpdater;
     }
 
     public async Task<Unit> Handle(UnassignPaymentCommand command, CancellationToken cancellationToken)
     {
         var existingAssignment = await _assignments.FirstAsync(ass => ass.Id == command.PaymentAssignmentId
+                                                                   && ass.Registration!.EventId == command.EventId
                                                                    && (ass.IncomingPaymentId != null || ass.OutgoingPaymentId != null), cancellationToken);
         if (existingAssignment.PaymentAssignmentId_Counter != null)
         {
@@ -69,6 +75,8 @@ public class UnassignPaymentCommandHandler : IRequestHandler<UnassignPaymentComm
                                   RegistrationId = existingAssignment.RegistrationId
                               });
         }
+
+        _readModelUpdater.TriggerUpdate<RegistrationCalculator>(existingAssignment.RegistrationId, command.EventId);
 
         return Unit.Value;
     }
