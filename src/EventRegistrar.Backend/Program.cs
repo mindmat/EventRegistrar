59f720e4eda291312b1a4870c826843e2c1800d6
Mathias Minder <mathias.minder@gmail.com>
2022-12-04 23:24:12 +0100

    fix read config condition

--- src/EventRegistrar.Backend/Program.cs
+++ src/EventRegistrar.Backend/Program.cs
@@ -143,7 +143,7 @@ container.RegisterSingleton<IDateTimeProvider, DateTimeProvider>();
 container.RegisterSingleton(() =>
 {
     var cs = builder.Configuration.GetValue<string>("ServiceBus_ConnectionString");
-    return cs == null
+    return cs != null
                ? new ServiceBusClient(cs)
                : new ServiceBusClient(builder.Configuration.GetValue<string>("ServiceBusNamespace"), new DefaultAzureCredential());
 });
