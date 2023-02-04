b5d01a5b9295b2cd51e9dfb37ac0309a3d5bde81
Mathias Minder <mathias.minder@gmail.com>
2023-02-04 10:36:42 +0100

    Allow changes to registrables when event is open

--- src/EventRegistrar.Backend/Registrables/SaveRegistrableCommand.cs
+++ src/EventRegistrar.Backend/Registrables/SaveRegistrableCommand.cs
@@ -1,6 +1,5 @@
 ﻿using EventRegistrar.Backend.Events;
 using EventRegistrar.Backend.Infrastructure;
-using EventRegistrar.Backend.Infrastructure.DataAccess;
 using EventRegistrar.Backend.Infrastructure.DataAccess.ReadModels;
 
 namespace EventRegistrar.Backend.Registrables;
@@ -19,45 +18,73 @@ public class SaveRegistrableCommand : IRequest, IEventBoundRequest
     public string? Tag { get; set; }
 }
 
-public class SaveRegistrableCommandHandler : IRequestHandler<SaveRegistrableCommand>
+public class SaveRegistrableCommandHandler : AsyncRequestHandler<SaveRegistrableCommand>
 {
     private readonly IRepository<Registrable> _registrables;
     private readonly IQueryable<Event> _events;
     private readonly ReadModelUpdater _readModelUpdater;
+    private readonly EnumTranslator _enumTranslator;
 
     public SaveRegistrableCommandHandler(IRepository<Registrable> registrables,
                                          IQueryable<Event> events,
-                                         ReadModelUpdater readModelUpdater)
+                                         ReadModelUpdater readModelUpdater,
+                                         EnumTranslator enumTranslator)
     {
         _registrables = registrables;
         _events = events;
         _readModelUpdater = readModelUpdater;
+        _enumTranslator = enumTranslator;
     }
 
-    public async Task<Unit> Handle(SaveRegistrableCommand command, CancellationToken cancellationToken)
+    protected override async Task Handle(SaveRegistrableCommand command, CancellationToken cancellationToken)
     {
         if (string.IsNullOrWhiteSpace(command.Name))
         {
             throw new ArgumentNullException(nameof(SaveRegistrableCommand.Name));
         }
 
+        var @event = await _events.FirstAsync(evt => evt.Id == command.EventId, cancellationToken);
         var registrable = await _registrables.AsTracking()
                                              .Include(rbl => rbl.Spots)
-                                             .FirstOrDefaultAsync(rbl => rbl.Id == command.RegistrableId, cancellationToken)
-                       ?? _registrables.InsertObjectTree(new Registrable
+                                             .FirstOrDefaultAsync(rbl => rbl.Id == command.RegistrableId, cancellationToken);
+        if (registrable == null)
+        {
+            registrable = _registrables.InsertObjectTree(new Registrable
                                                          {
                                                              Id = command.RegistrableId,
-                                                             EventId = command.EventId
+                                                             EventId = command.EventId,
+                                                             Type = command.Type
                                                          });
-        if (registrable.EventId != command.EventId)
-        {
-            throw new InvalidOperationException("Invalid EventID");
         }
-
-        var @event = await _events.FirstAsync(evt => evt.Id == command.EventId, cancellationToken);
-        if (@event.State is not RegistrationForms.EventState.Setup and not RegistrationForms.EventState.RegistrationOpen)
+        else
         {
-            throw new Exception($"To change a registrable, event must be in state setup or open, but it is in state {@event.State}");
+            if (registrable.EventId != command.EventId)
+            {
+                throw new InvalidOperationException("Invalid EventId");
+            }
+
+            if (registrable.Type != command.Type)
+            {
+                if (@event.State != RegistrationForms.EventState.Setup)
+                {
+                    throw new Exception($"To change the type of a track, event must be in state setup, but it is in state {_enumTranslator.Translate(@event.State)}");
+                }
+
+                var hasSpots = registrable.Spots?.Any(spt => !spt.IsCancelled) == true;
+                if (hasSpots)
+                {
+                    throw new Exception($"To change the type of a track, there must not be any registrations in it, but there are {registrable.Spots?.Count}");
+                }
+
+                var hasSpotsOnWaitingList = registrable.Spots?.Any(spt => spt is { IsCancelled: false, IsWaitingList: true }) == true;
+                if (registrable.HasWaitingList && !command.HasWaitingList && hasSpotsOnWaitingList)
+                {
+                    throw new Exception("Waiting list cannot be removed because there are spots on the waiting list");
+                }
+
+
+                registrable.Type = command.Type;
+            }
         }
 
         registrable.Name = command.Name;
@@ -67,20 +94,6 @@ public class SaveRegistrableCommandHandler : IRequestHandler<SaveRegistrableComm
                                             .Append(registrable.NameSecondary)
                                             .StringJoinNullable(" - ");
         registrable.Tag = command.Tag;
-        if (registrable.Type != command.Type)
-        {
-            if (@event.State != RegistrationForms.EventState.Setup)
-            {
-                throw new Exception($"To change the type of a track, event must be in state setup, but it is in state {@event.State}");
-            }
-
-            if (registrable.Spots?.Any() == true)
-            {
-                throw new Exception($"To change the type of a track, there must not be any registrations in it, but there are {registrable.Spots.Count}");
-            }
-
-            registrable.Type = command.Type;
-        }
 
         switch (registrable.Type)
         {
@@ -88,19 +101,17 @@ public class SaveRegistrableCommandHandler : IRequestHandler<SaveRegistrableComm
                 registrable.MaximumSingleSeats = command.MaximumSingleSpots;
                 registrable.MaximumDoubleSeats = null;
                 registrable.MaximumAllowedImbalance = null;
-                registrable.HasWaitingList = command.HasWaitingList && command.MaximumSingleSpots != null;
+                registrable.HasWaitingList = command is { HasWaitingList: true, MaximumSingleSpots: { } };
                 break;
 
             case RegistrableType.Double:
                 registrable.MaximumSingleSeats = null;
                 registrable.MaximumDoubleSeats = command.MaximumDoubleSpots;
                 registrable.MaximumAllowedImbalance = command.MaximumAllowedImbalance;
-                registrable.HasWaitingList = command.HasWaitingList && command.MaximumDoubleSpots != null;
+                registrable.HasWaitingList = command is { HasWaitingList: true, MaximumDoubleSpots: { } };
                 break;
         }
 
         _readModelUpdater.TriggerUpdate<RegistrablesOverviewCalculator>(null, command.EventId);
-
-        return Unit.Value;
     }
 }
