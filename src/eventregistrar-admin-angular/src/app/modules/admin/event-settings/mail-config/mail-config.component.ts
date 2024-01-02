956d21d55ff361903bb88aa371a3eb13fdfb150c
Mathias Minder <mathias.minder@outlook.com>
2024-01-02 22:43:36 +0100

    fix date format in input

--- src/eventregistrar-admin-angular/src/app/modules/admin/event-settings/mail-config/mail-config.component.ts
+++ src/eventregistrar-admin-angular/src/app/modules/admin/event-settings/mail-config/mail-config.component.ts
@@ -4,11 +4,38 @@ import { ExternalMailConfigurationDisplayItem, ExternalMailConfigurationUpdateIt
 import { Subject, merge, takeUntil } from 'rxjs';
 import { MailConfigService } from './mail-config.service';
 import { v4 as createUuid } from 'uuid';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material/core';
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+export const DE_FORMATS = {
+  parse: {
+    dateInput: 'DD.MM.YYYY',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+    monthYearLabel: 'DD.MM.YYYY',
+    dateA11yLabel: 'DD.MM.YYYY',
+    monthYearA11yLabel: 'DD.MM.YYYY',
+  },
+};
 
 @Component({
   selector: 'app-mail-config',
   templateUrl: './mail-config.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: DE_FORMATS }
+  ]
 })
 export class MailConfigComponent implements OnInit
 {
